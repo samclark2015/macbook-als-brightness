f3e7a195ccd2d84715939bfc2d669216c699a1a5
Sam Clark <slc2015@icloud.com>
2018-03-13 19:57:46 -0400

    Added installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+cp ./brightness.py /usr/local/bin/brightness.py
+chmod +x /usr/local/bin/brightness.py
+
+cp ./brightness.service /etc/systemd/system/brightness.service
+chmod +x /etc/systemd/system/brightness.service
